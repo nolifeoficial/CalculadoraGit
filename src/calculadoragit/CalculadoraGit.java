1e8223713c48b483b1fc23ae3845d7e9c2e53287
alexg <alexg@DESKTOP-3VIB73M>
2020-08-23 22:09:26 -0300

    variáveis da classe CalculadoraGit criadas: x, y, soma, subtracao, multiplicacao, divisao

--- src/calculadoragit/CalculadoraGit.java
+++ src/calculadoragit/CalculadoraGit.java
@@ -2,6 +2,11 @@ package calculadoragit;
 
 public class CalculadoraGit {
 
+    /**
+     * Declarar variáveis de classe
+     */
+    static int x, y, soma, subtracao, multiplicacao, divisao;
+    
     public static void main(String[] args) {
 
     }
